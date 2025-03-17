1ae01a721b4d9fa2a7b17072e8637b4c4ac9ab40
João Paulo Correa <jopa.correa27@gmail.com>
2025-03-17 16:50:04 -0600

    Add summary route

--- soundwise/src/App.jsx
+++ soundwise/src/App.jsx
@@ -5,9 +5,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
 import Home from "./Pages/Home/Home.jsx";
-import PageNotFound from "./Pages/404/PageNotFound.jsx";
-import Playlists from "./Pages/Playlists/Playlists.jsx";
 import Auth from "./Pages/Auth/Auth.jsx";
+import Playlists from "./Pages/Playlists/Playlists.jsx";
+import Summary from "./Pages/Summary/Summary.jsx";
+import PageNotFound from "./Pages/404/PageNotFound.jsx";
 
 // style
 import "./App.scss";
@@ -19,6 +20,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/playlists" element={<Playlists />} />
+        <Route path="/summary" element={<Summary />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
     </BrowserRouter>
