141661992ca5f1f6bbc05ec7bf12e95cbcb2f7c2
João Paulo Correa <jopa.correa27@gmail.com>
2025-03-19 19:39:53 -0600

    Add background animated

--- soundwise/src/App.jsx
+++ soundwise/src/App.jsx
@@ -1,6 +1,6 @@
-//App.jsx
+// App.jsx
 // npm libraries
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 // components
@@ -16,6 +16,8 @@ import Transfer from "./Pages/Transfer/Transfer";
 import "./App.scss";
 
 function App() {
+  const interactiveBubbleRef = useRef(null);
+
   useEffect(() => {
     window.checkMusicKit = () => {
       console.log(
@@ -33,9 +35,73 @@ function App() {
     };
   }, []);
 
+  // Set up the interactive bubble animation
+  useEffect(() => {
+    if (!interactiveBubbleRef.current) return;
+
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+    
+    function move() {
+      curX += (tgX - curX) / 20;
+      curY += (tgY - curY) / 20;
+      
+      if (interactiveBubbleRef.current) {
+        interactiveBubbleRef.current.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+      }
+      
+      requestAnimationFrame(() => {
+        move();
+      });
+    }
+    
+    const handleMouseMove = (event) => {
+      tgX = event.clientX;
+      tgY = event.clientY;
+    };
+    
+    window.addEventListener('mousemove', handleMouseMove);
+    move();
+    
+    // Cleanup function
+    return () => {
+      window.removeEventListener('mousemove', handleMouseMove);
+    };
+  }, [interactiveBubbleRef]); 
+
   return (
     <AuthProvider>
       <BrowserRouter>
+        <div className="gradient-bg">
+          <svg xmlns="http://www.w3.org/2000/svg">
+            <defs>
+              <filter id="goo">
+                <feGaussianBlur
+                  in="SourceGraphic"
+                  stdDeviation="10"
+                  result="blur"
+                />
+                <feColorMatrix
+                  in="blur"
+                  mode="matrix"
+                  values="1 0 0 0 0  0 1 0 0 0  0 0 1 0 0  0 0 0 18 -8"
+                  result="goo"
+                />
+                <feBlend in="SourceGraphic" in2="goo" />
+              </filter>
+            </defs>
+          </svg>
+          <div className="gradients-container">
+            <div className="g1"></div>
+            <div className="g2"></div>
+            <div className="g3"></div>
+            <div className="g4"></div>
+            <div className="g5"></div>
+            <div className="interactive" ref={interactiveBubbleRef}></div>
+          </div>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/auth" element={<Auth />} />
--- soundwise/src/Pages/Home/Home.jsx
+++ soundwise/src/Pages/Home/Home.jsx
@@ -2,12 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import "./Home.scss";
+
 function Home() {
   return (
-    <div>
-      <h1>SoundWise</h1>
+    <div className="home-component">
+      <h1 className="home-component__title">SoundWise</h1>
       <Link to="/auth">
-        <h2>Login with Spotify</h2>
+        <h2 className="home-component__subtitle">start</h2>
       </Link>
     </div>
   );
