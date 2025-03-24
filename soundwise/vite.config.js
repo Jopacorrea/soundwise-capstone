aa3011e0347c1f6c28bccdb6d91f9fe591d4ae51
João Paulo Correa <jopa.correa27@gmail.com>
2025-03-24 01:02:45 -0600

    adjust base vite.config to deploy

--- soundwise/vite.config.js
+++ soundwise/vite.config.js
@@ -1,9 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   base: '/',
+//   publicDir: 'public'
+// })
+
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
 export default defineConfig({
   plugins: [react()],
+  // This ensures that the base path is set correctly
   base: '/',
-  publicDir: 'public'
-})
+  build: {
+    // Ensure that the outDir is correctly set
+    outDir: 'dist',
+  },
+});
