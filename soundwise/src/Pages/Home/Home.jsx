c1d6f5dd5d9055060f3eaefbd2010d1043137112
João Paulo Correa <jopa.correa27@gmail.com>
2025-03-23 23:09:42 -0600

    add public image folder to deploy

--- soundwise/src/Pages/Home/Home.jsx
+++ soundwise/src/Pages/Home/Home.jsx
@@ -2,14 +2,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-import logoImage from "../../assets/images/Soundwise_Logo.png";
+//import logoImage from "../../assets/images/Soundwise_Logo.png";
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="home-component">
       <img
-        src={logoImage}
+        src="/images/Soundwise_Logo.png"
+        alt="Soundwise logo"
         className="home-component__logo"
       />
       <h1 className="home-component__title">SoundWise</h1>
