df14f7b65ee3fe0c84464c59efbf387564d49491
João Paulo Correa <jopa.correa27@gmail.com>
2025-03-23 23:05:12 -0600

    adjust img path for production - test

--- soundwise/src/Pages/Home/Home.jsx
+++ soundwise/src/Pages/Home/Home.jsx
@@ -2,13 +2,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import logoImage from "../../assets/images/Soundwise_Logo.png";
 import "./Home.scss";
 
 function Home() {
   return (
     <div className="home-component">
       <img
-        src="/src/assets/images/Soundwise_Logo.png"
+        src={logoImage}
         className="home-component__logo"
       />
       <h1 className="home-component__title">SoundWise</h1>
